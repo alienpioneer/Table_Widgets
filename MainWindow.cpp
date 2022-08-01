b6b9ba5168ca38840ab8069281599a0f3cdf268b
Alex Alexandrescu <alex.alexandresq@gmail.com>
2022-08-01 11:11:33 +0200

    modifications to the model

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -14,69 +14,66 @@ MainWindow::MainWindow(QWidget *parent)
 
     QList<QString> headers = { "A", "B", "C", "D"};
 
+///---------------------------------------------------- TABLE VIEW ---------------------------------------------------------
 
     QTableView* tableView = new QTableView(this);
+    tableView->setStyleSheet("QTableView QHeaderView { color: white;qproperty-highlightSections: false; }\
+                             QTableView QHeaderView::section {padding: 0 4px; background-color: #999;  border: 1px solid #777;}\
+                             QTableView QHeaderView::section:horizontal { height:28px;border-left:1px solid #AAA; border-top:none; }\
+                             QTableView QHeaderView::section:horizontal:last { border-right: none; }\
+                             QTableView QHeaderView::section:vertical { border-top: 1px solid #AAA; border-left:none; }\
+                             QTableView QHeaderView::section:vertical:last  { border-bottom: none; }\
+                             QScrollBar {background-color: #D8D8D8;border:none; } \
+                             QScrollBar::handle {border: 1px solid #999;border-bottom-color: #666;border-right-color:#666;border-radius: 4px;\
+                                                 background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #E4E4E4, stop:0.5 #D9D9D9, stop:0.501 #D3D3D3) }");
+
+    tableWidget->layout()->addWidget(tableView);
+
+    tableView->setSelectionMode( QAbstractItemView::NoSelection );
+    tableView->setEditTriggers( QAbstractItemView::NoEditTriggers );
+
+    tableView->verticalHeader()->hide();
+    //table->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+    //table->horizontalHeader()->setSectionResizeMode(QHeaderView::Fixed);
+    tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    tableView->horizontalHeader()->setSectionsClickable(false);
+    tableView->horizontalHeader()->setDefaultAlignment( Qt::AlignCenter | Qt::AlignVCenter );
+    tableView->setSizeAdjustPolicy( QAbstractScrollArea::AdjustToContents );
+
+    TableModel* model = new TableModel(this);
+    tableView->setModel(model);
+
+    // Call these functions directly on the custom the model
+    // The table->model() will return an abstract model
+    model->setHeaderLabels(headers, Qt::Horizontal);
+    model->setAltColumnColor(QColor(250,232,122));
+    model->useAltColumnColor(true);
+    model->useFirstColumnColor(true);
+
+    auto t0 = std::chrono::high_resolution_clock::now();
+
+    model->insertRows(0,2000, QModelIndex());
+    for( int i =0; i < 2000; i++ )
     {
-//    table->setStyleSheet("QTableView {border: 1px solid #666;gridline-color: #DDD;qproperty-iconSize: 48px 48px;} "\
-//                         "QTableView QHeaderView::section:horizontal{ border-left: 1px solid #AAA; border-top:none; } "\
-//                         "QTableView QHeaderView::section:horizontal:last  { border-right: none; } "\
-//                         "QScrollBar {background-color: #D8D8D8;border:none; } "\
-//                         "QScrollBar::handle {border: 1px solid #999;border-bottom-color: #666;border-right-color:#666;border-radius: 4px;"\
-//                         "background-color: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #E4E4E4, stop:0.5 #D9D9D9, stop:0.501 #D3D3D3 }");
-        tableView->setStyleSheet("QTableView QHeaderView { color: white;qproperty-highlightSections: false; }\
-                                  QTableView QHeaderView::section {padding: 0 4px; background-color: #999;  border: 1px solid #777;}\
-                                  QTableView QHeaderView::section:horizontal { height:28px;border-left:1px solid #AAA; border-top:none; }\
-                                  QTableView QHeaderView::section:horizontal:last { border-right: none; }\
-                                  QTableView QHeaderView::section:vertical { border-top: 1px solid #AAA; border-left:none; }\
-                                  QTableView QHeaderView::section:vertical:last  { border-bottom: none; }");
-
-        tableWidget->layout()->addWidget(tableView);
-
-        tableView->setSelectionMode( QAbstractItemView::NoSelection );
-        tableView->setEditTriggers( QAbstractItemView::NoEditTriggers );
-
-        tableView->verticalHeader()->hide();
-        //table->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
-        //table->horizontalHeader()->setSectionResizeMode(QHeaderView::Fixed);
-        tableView->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
-        tableView->horizontalHeader()->setSectionsClickable(false);
-        tableView->horizontalHeader()->setDefaultAlignment( Qt::AlignCenter | Qt::AlignVCenter );
-        tableView->setSizeAdjustPolicy( QAbstractScrollArea::AdjustToContents );
-
-        TableModel* model = new TableModel(this);
-        tableView->setModel(model);
-
-        // Call these functions directly on the custom the model
-        // The table->model() will return an abstract model
-        model->setHeaderLabels(std::move(headers), Qt::Horizontal);
-        model->setAltColumnColor(QColor(250,232,122));
-        model->useAltColumnColor(true);
-        model->useFirstColumnColor(true);
-
-        auto t0 = std::chrono::high_resolution_clock::now();
-
-        model->insertRows(0,2000, QModelIndex());
-        for( int i =0; i < 2000; i++ )
+        for( int j =0; j < 4; j++ )
         {
-            for( int j =0; j < 4; j++ )
-            {
-                model->setCellData(i,j,QString::number(i));
-                model->setCellColor(i,j,QColor(255,123,124));
-            }
+            model->setCellData(i,j,QString::number(i));
+            model->setCellColor(i,j,QColor(255,123,124));
         }
-
-        auto t1 = std::chrono::high_resolution_clock::now();
-        qDebug() << "Table Updates took" <<  std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() << " ms" ;
-
-        //model->clearTable();
-
-        // Set the colors at the beggining
-        //model->setFirstColumnColor(QColor(0,255,0));
-        //model->setAltColumnColor(QColor(0,0,255));
-        //model->useAltColumnColor(false);
-        //model->useFirstColumnColor(false);
-
     }
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    qDebug() << "Table Updates took" <<  std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0).count() << " ms" ;
+
+    //model->clearTable();
+
+    // Set the colors at the beggining
+    //model->setFirstColumnColor(QColor(0,255,0));
+    //model->setAltColumnColor(QColor(0,0,255));
+    //model->useAltColumnColor(false);
+    //model->useFirstColumnColor(false);
+
+///---------------------------------------------------- TABLE WIDGET ---------------------------------------------------------
 
 //    QTableWidget* table = new QTableWidget(this);
 //    tableWidget->layout()->addWidget(table);
